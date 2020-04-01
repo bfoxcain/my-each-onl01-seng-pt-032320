606b705e03792a182b5658e881c042a1185aeb3e
Beverly Appleton <bfcain19@gmail.com>
2020-04-01 03:22:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,9 @@
-def my_each(collection) # put argument(s) here
+def my_each(collections) # put argument(s) here
   i = 0
-  while i < word.length
-    yield(words[i])
+  while i < collections.length
+    yield(collections[i])
     i = i + 1
   end # code here
 end
+
+
